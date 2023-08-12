58c7508e3d8f2a668adb070f1ce826761c2c45bd
Tristan Simpson <75189508+realTristan@users.noreply.github.com>
2023-08-12 11:29:55 -0400

    Add files via upload

---
+++ src/parser/parser.h
@@ -0,0 +1,14 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "../lexer/lexer.h"
+#include <stdlib.h>
+
+/**
+ * @brief Parses tokens.
+ *
+ * @param token_array The tokens array.
+ */
+void parse(token_array_t *token_array);
+
+#endif // PARSER_H
